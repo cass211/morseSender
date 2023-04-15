1941df798bf601b5c9341ceae485a2f9e382d62a
Jim <jim@jam.sessionsnet.org>
2023-04-15 10:35:00 -0700

    comment out arg parsing

--- streamtest.py
+++ streamtest.py
@@ -15,9 +15,9 @@ for n in range(wavetable_length):
 
 #CHUNK = 1024
 
-if len(sys.argv) < 2:
-    print("Plays a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
-    sys.exit(-1)
+#if len(sys.argv) < 2:
+#    print("Plays a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
+#    sys.exit(-1)
 
 total_samples = round(t * sample_rate)
 
